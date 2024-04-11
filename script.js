398ecdebd1f927ca4d94712b1830bac0db555817
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2024-04-11 23:47:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('pink-button').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#f9dada'; 
+});
+
+document.getElementById('green-button').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#d3f9da';
+});
+
+document.getElementById('grey-button').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#f2f2f2'; 
+});
